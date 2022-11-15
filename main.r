92b3a5041a3271712756337be56ec39d72df9202
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2022-11-15 11:18:43 +0000

    Use same colors on chart and legend + Extend y axis range on barchart

--- main.r
+++ main.r
@@ -27,19 +27,26 @@ repository_by_archive_df <- repository_df %>%
   group_by(archive_status = repository_df$archived) %>%
   summarise(total = n())
 
+
+pie_colors <- rainbow(2) # c('blue', 'red') , terrain.colors(2), heat.colors, topo.colors, ...
 png("plot-repositories-by-archive-status.png")
-pie(repository_by_archive_df$total, labels = repository_by_archive_df$total, main = "Repositories by archive status")
-legend("topleft", legend = repository_by_archive_df$archive_status)
+pie(repository_by_archive_df$total, labels = repository_by_archive_df$total, main = "Repositories by archive status", col = pie_colors)
+legend("topleft", legend = c('Archived', 'Active'), fill = pie_colors)
 dev.off()
 
 repository_by_language_df <- active_repos %>%
   group_by(language_group = active_repos$language) %>%
-  count(sort = TRUE)
+  filter(language_group != "NULL") %>%
+  count(sort = TRUE) %>%
+  head(20)
 
 head(repository_by_language_df)
 
 png("plot-active-repositories-by-programming-language.png")
-barplot(repository_by_language_df$n, names.arg = repository_by_language_df$language_group, xlab = "", ylab = "Repository count", las=2, col = "skyblue", main="Active repositories by programming language")
+barplot(repository_by_language_df$n, names.arg = repository_by_language_df$language_group, xlab = "", ylab = "Repository count", las=2, col = "skyblue",
+        main="Programming language count for active repositories",
+        ylim=range(pretty(c(0, repository_by_language_df$n)))
+)
 dev.off()
 
 View(repository_by_language_df)
